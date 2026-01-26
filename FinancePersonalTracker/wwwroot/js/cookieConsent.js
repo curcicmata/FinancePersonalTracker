0a30de046bf11bd733a1ccb0547755b54782a022
Matej Čurčić <matej.curcic@pontistechnology.com>
2026-01-26 16:11:50 +0100

    cookie banner consent

---
+++ FinancePersonalTracker/wwwroot/js/cookieConsent.js
@@ -0,0 +1,20 @@
+﻿window.cookieConsent = {
+    accept: function () {
+        localStorage.setItem("cookieConsent", "granted");
+
+        if (typeof gtag === "function") {
+            gtag('consent', 'update', {
+                ad_storage: 'granted',
+                analytics_storage: 'granted'
+            });
+        }
+    },
+
+    decline: function () {
+        localStorage.setItem("cookieConsent", "denied");
+    },
+
+    check: function () {
+        return localStorage.getItem("cookieConsent");
+    }
+};
